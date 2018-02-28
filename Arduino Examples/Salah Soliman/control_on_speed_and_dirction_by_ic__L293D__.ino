3e5abc1476da1093c6be5fd864b97dcd97953596
salah14 <36545687+salah14@users.noreply.github.com>
2018-02-28 18:27:44 +0200

    Add files via upload

---
+++ Arduino Examples/Salah Soliman/control_on_speed_and_dirction_by_ic__L293D__.ino
@@ -0,0 +1,28 @@
+int control=A1;
+int dir1=40 , dir2=42 ; 
+int motor=10;
+int sp;
+int sw=38;
+void setup()
+{
+  pinMode(dir1,OUTPUT);
+  pinMode(dir2,OUTPUT);
+  pinMode(sw,INPUT);
+}
+void loop()
+{
+  if (digitalRead(sw)==HIGH)
+   {
+    digitalWrite(dir2,LOW);
+    delay (1000);
+    digitalWrite(dir1,HIGH); 
+   }
+  else
+   {
+     digitalWrite(dir1,LOW);
+    delay (1000);   
+    digitalWrite(dir2,HIGH);
+   }
+   sp=analogRead(control)/4.0;
+   analogWrite(motor,sp);
+}
